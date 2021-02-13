cf00b380dbb385d0d6311d0413de134a15909629
saddamarbaa <saddamarbaasaddamarbaa@gmail.com>
2021-02-14 04:41:57 +0700

    ADD:seedrandom & random dependencies

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const _ = require("underscore");
+const seedrandom = require("seedrandom");
 
 let total = _.indexOf([1, 2, 3, 4], 2);
 let tota2 = _.indexOf([1, 2, 3, 4], 4);
+
 console.log(tota2);
 console.log(total);
+
+let rng = seedrandom("hello.");
+console.log(rng());
--- install.md
+++ install.md
@@ -3,3 +3,7 @@
 -- npm- Package Manager
 
 -- npm install mongoose
+
+-- npm install --save random
+
+-- npm install seedrandom
